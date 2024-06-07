e1b12b544f721f36ae55dccc9ba843120e71c48a
Max Martini <160190640+mmartini05@users.noreply.github.com>
2024-06-07 14:33:42 -0400

    currently displays number for ABXY buttons
    not sure how to differentiate

--- main.ino
+++ main.ino
@@ -1,9 +1,15 @@
+#include <USBHost_t36.h>
+
 // V+ from USB-C goes to 5V on board
 // V- from USB-C goes to GND (any)
 
 // 5V (Yellow) line for GCC is unused currently
 // 3.3 (Blue) line for GCC is from the onboard 3.3V pin
 // GND (Green/White) line for GCC goes to GND (any)
+
+USBHost SPC; // Sets up SPC as the USB Host
+USBHIDParser hid(SPC); // HID input decoded with USBHIDParser
+JoystickController joysticks[1] = {JoystickController(SPC)}; // 1 joystick being used (testing)
 
 // SPC Controls
 
@@ -59,44 +65,16 @@ bool gcc_Z;
 int gcc_LTRIG; // L/R triggers maybe should be bool? regardless, SPC L/R trig = bool, so GCC L/R trig = bool
 int gcc_RTRIG;
 
-uint8_t high[40]; // Data+ from USB-C
-uint8_t low[40]; // Data- from USB-C
-
 void setup() { // Initalization
   Serial.begin(9600);
-  pinMode(1, INPUT); // Data+ is connected to Pin 1 / Port D3
-  pinMode(2, INPUT); // Data- is connected to Pin 2 / Port D4
-  pinMode(31, OUTPUT); // Gamecube Data is connected to Pin 31 / Port PD1 - this is TX (UART)
+  while(!Serial); // Waits for serial monitor to open
+  Serial.println("Begin");
+  SPC.begin();
 }
 
 void loop() { // Loops continuously
-  for (int i = 0; i < 40; i++) {
-    high[i] = digitalRead(1);
-    low[i] = digitalRead(2);
-    Serial.print(differential(high[i], low[i]), HEX); // Testing to see if I can read a byte from USB-C Data+/Data-
-    Serial.print(' ');
-    if (i % 10 == 0) {
-      Serial.println();
-    }
-    if (i % 40 == 0) {
-      Serial.println();
-    }
-    delay(200);
-  }
-}
-
-uint8_t differential(uint8_t high, uint8_t low) { // This function takes 40 Data+ bytes and 40 Data- bytes from the USB-C and combines into a 40 Data bytes for the GCC
-  uint8_t diff = 0; // Initializes diff (differential byte) to zero
-  bool hBit;
-  bool lBit;
-
-  for (int i = 0; i < 8; i++) { // Loop through all 8 bits of the byte
-    hBit = (high >> i) & 0x01; // Extract the ith bit of high
-    lBit = (low >> i) & 0x01;  // Extract the ith bit of low
-    
-    if (hBit > lBit) {
-      diff |= (1 << i); // Set the ith bit of diff to 1
-    }
-  }
-  return diff;
+  uint32_t buttons = joysticks[0].getButtons();
+  Serial.printf("Joystick(0): buttons = %x", buttons);
+  Serial.println();
+  delay(200);
 }
